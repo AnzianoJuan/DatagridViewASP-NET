9468f368f28943a82169827524ae940f1e494647
Juan anziano <juanianziano22@gmail.com>
2025-01-04 04:37:39 -0300

    Primer commit .... proxima actualizacion sera para agregar los controles en ele front , para que se puedavisualizar la DataGridView con la lista creada en la clase de dominio

---
+++ ejemplo-clase5-u4-Cs3/Dominio/Auto.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Auto
+    {
+        public int Id { get; set; }
+
+        public string Modelo { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public string Color { get; set; }
+
+        public DateTime Fecha { get; set; }
+
+        public bool Usado { get; set; }
+
+        public bool Importado { get; set; }
+    }
+
+    public class AutoNegocio
+    {
+        public List<Auto> listar()
+        {
+
+            List<Auto> listaDeAutos = new List<Auto>();
+
+            listaDeAutos.Add(new Auto());
+            listaDeAutos.Add(new Auto());
+
+            listaDeAutos[0].Id = 1;
+            listaDeAutos[0].Modelo = "Ferrari";
+            listaDeAutos[0].Descripcion = "EL tano nro1";
+            listaDeAutos[0].Color = "Rojo";
+            listaDeAutos[0].Fecha = DateTime.Now;
+            listaDeAutos[0].Usado = true;
+            listaDeAutos[0].Importado = true;
+
+
+            listaDeAutos[1].Id = 2;
+            listaDeAutos[1].Modelo = "Golf";
+            listaDeAutos[1].Descripcion = "EL de ordi";
+            listaDeAutos[1].Color = "Negro";
+            listaDeAutos[1].Fecha = DateTime.Now;
+            listaDeAutos[1].Usado = true;
+            listaDeAutos[1].Importado = true;
+
+            return listaDeAutos;
+
+        }
+
+    }
+}
